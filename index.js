66af6f4bcebd55fc8734490f27f6c212f6d5c180
morthos <pathshan61@gmail.com>
2018-08-31 14:20:51 -0400

    .

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var rightPressed = false;
-var leftPressed = false;
-var downPressed = false;
-var upPressed = false;
 const playerElement = document.getElementById("kratos")
 const mazeDiv = document.getElementById("maze")
+const currentPosition = {
+    top: 0,
+    left: 0,
+}
+const currentIndex = {
+    row: 0,
+    cell: 0,
+}
+
 const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
     "W   W     W     W W W",
@@ -22,17 +27,20 @@ const map = [
     "WWWWWWWWWWWWWWWWWWWWW"
 ];
 
-for (let rowIndex = 0; rowIndex < map.length; rowIndex++){ 
+for (let rowIndex = 0; rowIndex < map.length; rowIndex++) {
     const rowString = map[rowIndex]
     const rowDiv = document.createElement("div")
     rowDiv.classList.add("row")
     mazeDiv.appendChild(rowDiv)
 
-    for (let cellIndex = 0; cellIndex < rowString.length; cellIndex++){
+    for (let cellIndex = 0; cellIndex < rowString.length; cellIndex++) {
         const cell = rowString[cellIndex]
         switch (cell) {
             case "W":
-                rowDiv.appendChild(createCell("wall", rowIndex, cellIndex))
+                const wallCell = createCell("wall", rowIndex, cellIndex)
+                wallCell.dataset.type = "wall"
+                rowDiv.appendChild(wallCell)
+
                 break
             case " ":
                 rowDiv.appendChild(createCell("space", rowIndex, cellIndex))
@@ -41,12 +49,35 @@ for (let rowIndex = 0; rowIndex < map.length; rowIndex++){
                 const startCell = createCell("start", rowIndex, cellIndex)
                 rowDiv.appendChild(startCell)
                 startCell.appendChild(playerElement)
+                movePlayerToStart(startCell)
                 break
             case "F":
-                rowDiv.appendChild(createCell("finish", rowIndex, cellIndex))
+                const finishCell = createCell("finish", rowIndex, cellIndex)
+                finishCell.dataset.type = "finish"
+                rowDiv.appendChild(finishCell)
                 break
         }
     }
+}
+
+function getCoords(element) {
+    const box = element.getBoundingClientRect();
+
+    return {
+        top: box.top + window.pageXOffset,
+        left: box.left + window.pageYOffset
+    };
+}
+
+function movePlayerToStart(startCell) {
+    const startPosition = getCoords(startCell)
+    playerElement.style.top = startPosition.top + "px"
+    playerElement.style.left = startPosition.left + "px"
+
+    currentPosition.top = startPosition.top
+    currentPosition.left = startPosition.left
+    currentIndex.row = parseInt(startCell.dataset.rowIndex)
+    currentIndex.cell = parseInt(startCell.dataset.cellIndex)
 }
 
 function createCell(type, rowIndex, cellIndex) {
@@ -57,37 +88,48 @@ function createCell(type, rowIndex, cellIndex) {
     return cellElement
 }
 
-document.addEventListener('keydown', (event) => {
-    let currentCellElement = playerElement.parentElement
-    let currentRowIndex = currentCellElement.dataset.rowIndex
-    let currentCellIndex = currentCellElement.dataset.cellIndex
-    let nextRowIndex = currentRowIndex + 0
-    let nextCellIndex = currentCellIndex + 1
-    let keyName = event.key;
-    switch(keyName){
-        case "ArrowDown":
-        downPressed = true
-        currentRowIndex += 1
-        document.getElementById("kratos").style.top = nextRowIndex + "px"
-        break
+document.addEventListener('keydown', event => {
+    if (!event.key.includes("Arrow")) return false
 
-        case "ArrowUp":
-        upPressed = true
-        currentRowIndex += 1
-        document.getElementById("kratos").style.top = nextRowIndex + "px"
-        break
-
-        case "ArrowLeft":
-        leftPressed = true
-        currentCellIndex += 0
-        document.getElementById("kratos").style.top = nextCellIndex = "px"
-        break
-
-        case "ArrowRight":
-        rightPressed = true
-        currentCellIndex += 0
-        document.getElementById("kratos").style.top = nextCellIndex = "px"
+    const directionOffsets = {
+        "ArrowDown": {
+            vertical: +1,
+            horizontal: +0,
+        },
+        "ArrowUp": {
+            vertical: -1,
+            horizontal: +0,
+        },
+        "ArrowLeft": {
+            vertical: +0,
+            horizontal: -1,
+        },
+        "ArrowRight": {
+            vertical: +0,
+            horizontal: +1,
+        },
     }
-    console.log('keydown event\n\n' + 'key: ' + keyName);
 
+    const nextCurrentPositionTop = currentPosition.top + directionOffsets[event.key].vertical * 20
+    const nextCurrentPositionLeft = currentPosition.left + directionOffsets[event.key].horizontal * 20
+    const nextRowIndex = currentIndex.row + directionOffsets[event.key].vertical
+    const nextCellIndex = currentIndex.cell + directionOffsets[event.key].horizontal
+    const selector = "[data-row-index='" + nextRowIndex + "'][data-cell-index='" + nextCellIndex + "']"
+    const nextCellElement = document.querySelector(selector)
+    if (nextCellElement.dataset.type === "wall") return false
+
+    currentIndex.row = nextRowIndex
+    currentIndex.cell = nextCellIndex
+    currentPosition.top = nextCurrentPositionTop
+    currentPosition.left = nextCurrentPositionLeft
+    console.log(currentPosition)
+    playerElement.style.top = nextCurrentPositionTop + "px"
+    playerElement.style.left = nextCurrentPositionLeft + "px"
+    console.log('keydown event\n\n' + 'key: ' + event.key);
+    checkWin(nextCellElement)
 })
+function checkWin(nextCellElement){
+    if (nextCellElement.dataset.type === "finish") {
+        alert("You Win!")
+    }
+}
